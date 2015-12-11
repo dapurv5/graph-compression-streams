42d3cd643884fed999629c4d5d9a7b46b27195fa
Apurv Verma <dapurv5@gmail.com>
2015-12-11 16:02:50 -0500

    first commit

---
+++ src/main/java/org/gatech/graphcompression/App.java
@@ -0,0 +1,13 @@
+package org.gatech.graphcompression;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
